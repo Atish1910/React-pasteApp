d0814f8d3336d12c97377d9d1015c2f3d6f7cedb
Atish1910 <atishkamble398@gmail.com>
2025-03-25 22:54:58 +0530

    zxczcx

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Home from "./Home";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-function Paste(){
-    const pastes = useSelector((state) => state.pastes.Paste);
-    console.log(pastes)
+const Paste = () =>{
+    const pastes = useSelector((state) => state.paste.Pastes);
+    console.log(pastes);
+    
+    const [searchTerm, setSearchTerm] = useState("");
 
+    const dispatch = useDispatch();
+    const filteredData = pastes.filter(
+        (paste) => paste.title.toLowerCase().include(searchTerm.toLowerCase())
+    );
     return(
         <>
             <div className="container border py-5">
-                <div className="row border">
-                    <h1>List Of Pastes</h1>
+                <div className="row">
+                    <h1> hii I am Paste Container</h1>
+                    <div className="">
+                        <input 
+                            type="search"
+                            className="form-control"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            placeholder="Search Data With Title"
+                        />
+                    </div>
+                    <div className="">
+                        {
+                            filteredData.length > 0 &&
+                            filteredData.map((paste) =>{
+                                return(
+                                    <div className="">
+                                        {
+                                            paste.title
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </>
     )
 }
+
 export default Paste;
